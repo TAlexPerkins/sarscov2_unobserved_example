ad48d727f638cf39708b032183a722498cebb50b
slwu89 <slwu89@berkeley.edu>
2020-03-12 11:23:43 -0700

    summary functions

--- simOutbreak/src/simOutbreak.c
+++ simOutbreak/src/simOutbreak.c
@@ -69,27 +69,38 @@ SEXP simOutbreak_C(
   double symp_to_death_meanlog = log(symp_to_death_mean) - 0.5*pow(symp_to_death_sdlog,2);
 
   // initiate vector to store daily incidence of autochthonous infections
-  int* dailyIncidence = calloc(stopSimulationDay,sizeof(int));
+  int* dailyIncidence = (int*)malloc(stopSimulationDay*sizeof(int));
 
   // initiate vector to store daily reporting of autochthonous infections
-  int* dailyCases = calloc(stopSimulationDay,sizeof(int));
+  int* dailyCases = (int*)malloc(stopSimulationDay*sizeof(int));
 
   // initiate vector to store daily mortality of autochthonous infections
-  int* dailyMortality = calloc(stopSimulationDay,sizeof(int));
+  int* dailyMortality = (int*)malloc(stopSimulationDay*sizeof(int));
 
   // replicate repSims number of times
   for(int rr=0; rr<repSims; rr++){
 
+    // zero out arrays for this MC rep
+    for(int i=0; i<stopSimulationDay; i++){
+      dailyIncidence[i] = 0;
+      dailyCases[i] = 0;
+      dailyMortality[i] = 0;
+    }
+
     // initiate vector to store timing of parent infections
-    int* time_exp = calloc(timeImport_len,sizeof(int));
+    double* time_exp = (double*)malloc(timeImport_len*sizeof(double));
+    memcpy(time_exp,timeImport_ptr,timeImport_len*sizeof(double));
     int time_exp_len = timeImport_len;
+
+    // use this when we replace the variable of parents with offspring
+    double* time_exp_new = (double*)malloc(timeImport_len*sizeof(double));
 
     // loop through generations of transmission until extinct or time is up
     while(timeImport_len > 0){
 
       // draw a number of offspring for each parent
-      double* R_vec = malloc(sizeof(double)*time_exp_len);
-      int* number_offspring = malloc(sizeof(int)*time_exp_len);
+      double* R_vec = (double*)malloc(sizeof(double)*time_exp_len);
+      int* number_offspring = (int*)malloc(sizeof(int)*time_exp_len);
       int number_offspring_tot = 0; // total num off spring from all parents this gen
       for(int j=0; j<time_exp_len; j++){
         // get parent j's R0
@@ -104,7 +115,87 @@ SEXP simOutbreak_C(
 
       if(number_offspring_tot > 0){
 
-        
+        // remove parents with zero offspring
+        // we're not gonna do that in C due to too much memory management, it's slow
+        // we'll just check the logic below
+
+        // determine the time of exposure of each offspring
+        time_exp_new = (double*)realloc(time_exp_new,number_offspring_tot*sizeof(double));
+        int time_exp_new_i = 0;
+        if(lnormFlag==1){
+
+          // go over each parent
+          for(int j=0; j<time_exp_len; j++){
+            // if produced offspring cases, loop over them
+            if(number_offspring[j] > 0){
+              for(int k=0; k<number_offspring[j]; k++){
+                // k'th offspring of parent j has exposure time equal to time_exp[j] plus a random variate
+                time_exp_new[time_exp_new_i] = time_exp[j] + rnorm(si_mean,si_sd);
+                time_exp_new_i++;
+              }
+            }
+          }
+
+        } else {
+
+          // go over each parent
+          for(int j=0; j<time_exp_len; j++){
+            // if produced offspring cases, loop over them
+            if(number_offspring[j] > 0){
+              for(int k=0; k<number_offspring[j]; k++){
+                // k'th offspring of parent j has exposure time equal to time_exp[j] plus a random variate
+                time_exp_new[time_exp_new_i] = time_exp[j] + rlnorm(si_meanlog,si_sdlog);
+                time_exp_new_i++;
+              }
+            }
+          }
+
+        }
+
+        // C only: update time_exp
+        time_exp = (double*)realloc(time_exp,number_offspring_tot*sizeof(double));
+        memcpy(time_exp,time_exp_new,number_offspring_tot*sizeof(double));
+        time_exp_len = number_offspring_tot;
+
+        // retain only those infection that occur before time is up
+        int inf_within_bound = 0;
+        double* time_exp_within_bound = (double*)malloc(sizeof(double)*time_exp_len);
+        for(int j=0; j<time_exp_len; j++){
+          if( (int)floor(time_exp[j]) <= stopSimulationDay ){
+            time_exp_within_bound[inf_within_bound] = time_exp[j];
+            inf_within_bound++;
+          }
+        }
+        time_exp = (double*)realloc(time_exp,sizeof(double)*inf_within_bound);
+        memcpy(time_exp,time_exp_within_bound,sizeof(double)*inf_within_bound);
+        time_exp_len = inf_within_bound;
+
+        // determine incubation period and detection in a single loop
+        double* incubation_periods = (double*)malloc(time_exp_len*sizeof(double));
+        double* time_det = (double*)malloc(time_exp_len*sizeof(double));
+        for(int j=0; j<time_exp_len; j++){
+          incubation_periods[j] = rweibull(inc_shape,inc_scale);
+          time_det[j] = time_exp[j] + incubation_periods[j] + rpois(report_delay_rate);
+        }
+
+        // retain only those detected infections that occur before time is up
+        int det_within_bound_i = 0;
+        double* det_within_bound = (double*)malloc(sizeof(double)*time_exp_len);
+        for(int j=0; j<time_exp_len; j++){
+          if( (int)floor(time_det[j]) <= stopSimulationDay ){
+            det_within_bound[det_within_bound_i] = time_det[j];
+            det_within_bound_i++;
+          }
+        }
+        time_det = (double*)realloc(time_det,sizeof(double)*det_within_bound_i);
+        memcpy(time_det,det_within_bound,sizeof(double)*det_within_bound_i);
+
+        // update vector of daily incidence of detected infections
+
+
+
+
+
 
       }
 
@@ -118,6 +209,7 @@ SEXP simOutbreak_C(
 
 
     free(time_exp);
+    free(time_exp_new);
   } // end monte carlo reps
 
 
